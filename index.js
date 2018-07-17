04fa12ac80bb480994d3554ac4cbed586c562c47
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 13:14:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(){
-
-
+function doToElementsInArray(array, callbackFunction){
+  array.forEach(callbackFunction)
 }
 
 // Add your changeCompletely() function here:
